c2fd4a68c0e75fc0fddc5ffb5fe2868526595896
paradoxalyty <paradoxalyty@gmail.com>
2021-01-14 20:53:14 +0100

    asinc,await

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,47 @@
+import { useState, useEffect } from "react";
+
+const useContacts = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch("https://randomuser.me/api/?results=200");
+        const { results, error } = await response.json();
+        if (error) {
+          throw new Error(error);
+        }
+        setData(results);
+        setIsError(false);
+      } catch (e) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getContacts();
+  }, []);
+
+  return {
+    data,
+    isLoading,
+    isError,
+  };
+};
+
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  const contacts = useContacts();
+
+  if (contacts.isLoading) {
+    return <div>...loading</div>;
+  }
+
+  if (contacts.isError) {
+    return <div>...error</div>;
+  }
+
+  return <div>Contacts {contacts.data[0].name.first}</div>;
 };
