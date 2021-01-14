57ee3fdfd804bab42a4f9fba212ff1416a86d1e4
paradoxalyty <paradoxalyty@gmail.com>
2021-01-14 21:57:27 +0100

    Hook useContacts

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import { useContacts } from "./useContacts";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -10,38 +10,6 @@ const useStyles = makeStyles((theme) =>
     },
   })
 );
-
-const useContacts = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    const getContacts = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch("https://randomuser.me/api/?results=200");
-        const { results, error } = await response.json();
-        if (error) {
-          throw new Error(error);
-        }
-        setData(results);
-        setIsError(false);
-      } catch (e) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    getContacts();
-  }, []);
-
-  return {
-    data,
-    isLoading,
-    isError,
-  };
-};
 
 export const Contacts = () => {
   const classes = useStyles();
